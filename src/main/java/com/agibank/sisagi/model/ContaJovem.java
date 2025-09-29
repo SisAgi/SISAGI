286b0a0ea65cdda66b524d3715c457c8202c4aec
Nelson Damico Junior <neodamico@gmail.com>
2025-09-29 19:59:19 -0300

    feat(conta): renomeia ContaMenor para ContaJovem e vincula a conta a um responsável

---
+++ src/main/java/com/agibank/sisagi/model/ContaJovem.java
@@ -0,0 +1,21 @@
+package com.agibank.sisagi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@DiscriminatorValue("JOVEM")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ContaJovem extends Conta {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "conta_responsavel_id", referencedColumnName = "id_conta", nullable = false)
+    private Conta responsavel;
+
+    @Builder.Default
+    private boolean possuiLimite = false; // sempre falso
+}
