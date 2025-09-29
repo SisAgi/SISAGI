70cc95cd0a4d06454eda79d829f8f24fff3a73c3
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-09-29 10:18:10 -0300

    Create ContaPoupanca
    Criado a entidade Conta Poupança com seus atributos específico.
    
    - dataAniversario = responsável pela data que será efetuado o rendimento, adicionado como default para sempre ser a data de hoje para data de abertura da conta
    
    - rendimento = valor que será o rendimento, no caso estabelecemos o padrão de 0.05 ao mês.

---
+++ src/main/java/com/agibank/sisagi/model/ContaPoupanca.java
@@ -0,0 +1,26 @@
+package com.agibank.sisagi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@DiscriminatorValue("POUPANCA")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ContaPoupanca extends Conta{
+
+    @Column(name = "data_aniversario")
+    @Builder.Default
+    private LocalDate dataAniversario = LocalDate.now();
+
+    @Column(name = "rendimento")
+    @Builder.Default
+    private double rendimento = 0.05;
+}
