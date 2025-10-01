ff406b35bfd33d0d087711412efd7af0b7a8f572
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-09-30 21:02:38 -0300

    Create RecursoNaoEncontrado.java
    Criação de classe para tratamento de exceções de RuntimeException, com o construtor padrão herdando message da super classe

---
+++ src/main/java/com/agibank/sisagi/exception/RecursoNaoEncontrado.java
@@ -0,0 +1,7 @@
+package com.agibank.sisagi.exception;
+
+public class RecursoNaoEncontrado extends RuntimeException{
+    public RecursoNaoEncontrado(String message){
+        super(message);
+    }
+}
