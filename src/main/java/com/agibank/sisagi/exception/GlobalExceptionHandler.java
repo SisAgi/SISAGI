366e28395ebf0312b74c8eed619a4e78c61b2ab7
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-10-01 10:06:01 -0300

    Update GlobalExceptionHandler.java
    Adicionado tratamento de erro para JSON mal formado e para dados enviados de forma não esperada em nosso JSON

--- src/main/java/com/agibank/sisagi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/agibank/sisagi/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.agibank.sisagi.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -48,4 +50,34 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidJson(HttpMessageNotReadableException ex,
+                                                           HttpServletRequest request){
+        ErrorResponse error = new ErrorResponse("JSON Malformado",
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                request.getRequestURI()
+                );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                HttpServletRequest request)
+    {
+
+        String erros = ex.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() + ": "+ err.getDefaultMessage())
+                .reduce((a, b) -> a + "; \n" + b)
+                .orElse("Validação falhou");
+
+        ErrorResponse error = new ErrorResponse(erros,
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+    }
 }
