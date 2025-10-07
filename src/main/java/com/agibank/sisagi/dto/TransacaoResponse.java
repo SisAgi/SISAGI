6818f92d2254bd75896d8b88ada0b9583f796534
Vitor souza <113396211+litorx@users.noreply.github.com>
2025-10-07 09:28:04 -0300

    Update TransacaoResponse.java

--- src/main/java/com/agibank/sisagi/dto/TransacaoResponse.java
+++ src/main/java/com/agibank/sisagi/dto/TransacaoResponse.java
@@ -6,13 +6,11 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record TransacaoResponse(
-        // Campos obrigatórios conforme critérios de aceite
         Long id,
         TipoTransacao tipo,
         BigDecimal valor,
         LocalDateTime dataHora,
         
-        // Campos adicionais para enriquecer a resposta
         String nsUnico,
         Long contaOrigemId,
         String numeroContaOrigem,
