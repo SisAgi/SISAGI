fac46a70c3d4808cf4645f5e34d57bed0a58ad3f
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-10-13 11:45:05 -0300

    Update ContaCorrente.java
    Adicionado um método sobre escrito debitar com a logica do cheque especial para conta corrente

--- src/main/java/com/agibank/sisagi/model/ContaCorrente.java
+++ src/main/java/com/agibank/sisagi/model/ContaCorrente.java
@@ -1,5 +1,6 @@
 package com.agibank.sisagi.model;
 
+import com.agibank.sisagi.exception.SaldoInsuficienteException;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
@@ -18,4 +19,18 @@ public class ContaCorrente extends Conta {
     //Atributo específico da conta corrente
     @Column(name = "limite_cheque_especial", nullable = true, precision = 15, scale = 2)
     private BigDecimal limiteChequeEspecial;
+
+
+    public void debitarCorrente(BigDecimal valor) {
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("O valor do débito deve ser positivo.");
+        }
+        BigDecimal limiteTotal = getSaldo().add(this.limiteChequeEspecial);
+        if(valor.compareTo(limiteTotal) > 0){
+            throw new SaldoInsuficienteException("Limite e saldo insuficiente");
+        }
+        setSaldo(getSaldo().subtract(valor));
+    }
+
+
 }
