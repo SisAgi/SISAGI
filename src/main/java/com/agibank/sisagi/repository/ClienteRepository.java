808b0ea8ab125250092187278493756bd23c46eb
TheusLazaro <matheuslazaro13@outlook.com>
2025-10-14 16:00:43 -0300

    feat(graficos): prepara o sistema para o uso de gráficos em nosso dashboard

--- src/main/java/com/agibank/sisagi/repository/ClienteRepository.java
+++ src/main/java/com/agibank/sisagi/repository/ClienteRepository.java
@@ -1,15 +1,17 @@
 package com.agibank.sisagi.repository;
 
+import com.agibank.sisagi.dto.dashboard.ContasPorGerenteResponse;
 import com.agibank.sisagi.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-    //TODO: Implementar security para remover aviso
     Optional<Cliente> findByEmail(String email);
 
     Optional<Cliente> findById(Long id);
@@ -17,4 +19,7 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     Optional<Cliente> findByCpf(String cpf);
 
     Optional<Cliente> findByGerenteId(Long gerenteId);
+
+    @Query("SELECT new com.agibank.sisagi.dashboard.dto.ContasPorGerenteResponse(c.gerente.nome, COUNT(c)) FROM Cliente c WHERE c.gerente IS NOT NULL GROUP BY c.gerente.nome ORDER BY COUNT(c) DESC")
+    List<ContasPorGerenteResponse> countClientesByGerente();
 }
--- src/main/java/com/agibank/sisagi/repository/ContaRepository.java
+++ src/main/java/com/agibank/sisagi/repository/ContaRepository.java
@@ -1,11 +1,13 @@
 package com.agibank.sisagi.repository;
 
+import com.agibank.sisagi.dto.dashboard.ContasPorTipoResponse;
 import com.agibank.sisagi.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,4 +20,8 @@ public interface ContaRepository extends JpaRepository<Conta, Long> {
 
     @Query("SELECT c FROM Conta c JOIN c.titulares t WHERE t.cpf = :cpf")
     List<Conta> findByTitularCpf(@Param("cpf") String cpf);
+
+    Long countByDataAberturaBetween(LocalDate start, LocalDate end);
+    @Query("SELECT new com.agibank.sisagi.dashboard.dto.ContasPorTipoResponse(c.tipoConta, COUNT(c)) FROM Conta c GROUP BY c.tipoConta")
+    List<ContasPorTipoResponse> countContasByTipo();
 }
